e81531beb25c191ce2dd774a12751f843d7f5eda
Salvav <villegassalva94@gmail.com>
2023-09-05 13:21:30 -0600

    creater header #4

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+	constructor(private headerService: HeaderService){}
+
+	name : string = "";
+	titulos : string = "";
+	mision : string = "";
+	foto : string = "";
+
+	ngOnInit(): void {
+        this.headerService.getHeader()
+	.subscribe(  (data: any) =>{
+	console.log(data);
+	//alert(data);
+	this.name = data.name;
+	this.titulos = data.titulos;
+	this.mision = data.mision;
+	this.foto = data.foto;
+	});
+    }
 }
